e337ed1a276da74af4f5b3efda3918f91a2b0123
Dilenio Enderle <dilenio@gmail.com>
2020-07-14 11:22:12 -0300

    Primeiro exercício

---
+++ exercises/4_1/exercise1.js
@@ -0,0 +1,16 @@
+let a = 11;
+let b = 8;
+let res = a + b;
+console.log(res);
+
+res = a - b;
+console.log(res);
+
+res = a * b;
+console.log(res)
+
+res = a / b;
+console.log(res);
+
+res = a % b;
+console.log(res);
