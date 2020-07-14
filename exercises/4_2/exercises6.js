d1ab559665a0b1f87f1bedc5498858c12fa2f573
Dilenio Enderle <dilenio@gmail.com>
2020-07-14 18:09:51 -0300

    Sexto exercício

---
+++ exercises/4_2/exercises6.js
@@ -0,0 +1,15 @@
+// Descubra quantos valores ímpares existem no array e imprima o resultado.
+// Caso não exista nenhum, imprima a mensagem: "nenhum valor ímpar encontrado";
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let oddNumbers = 0;
+
+for (let i = 0; i < numbers.length; i++ ){
+  oddNumbers = (numbers[i] % 2 !== 0) ? oddNumbers += 1 : oddNumbers;
+  // Alternative to 
+  // if (numbers[i] % 2 !== 0){
+  //   oddNumbers += 1;
+  // }
+}
+
+console.log("Números ímpares: " + oddNumbers);
