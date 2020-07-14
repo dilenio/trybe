06ab034f68f7951353eedbbf0c1d13fc9e2bb6cb
Dilenio Enderle <dilenio@gmail.com>
2020-07-14 15:59:23 -0300

    Terceiro exercício

--- exercises/4_2/exercises2.js
+++ exercises/4_2/exercises2.js
@@ -1,4 +1,4 @@
-// Nesse primeiro exercício, percorra o array imprimindo todos os valores nele contidos com a função console.log();
+// Para o segundo exercício, você deve somar todos os valores contidos no array e imprimir o resultado;
 
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let sum = 0;
