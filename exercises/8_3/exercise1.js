9063815689b5d8612fdfc082e6b89a93bfcc4cca
Dilenio Enderle <dilenio@gmail.com>
2020-08-20 10:05:19 -0300

    Dada uma matriz de matrizes, transforme em uma única matriz.

---
+++ exercises/8_3/exercise1.js
@@ -0,0 +1,16 @@
+
+const assert = require('assert');
+
+const arrays = [
+  ["1", "2", "3"],
+  [true],
+  [4, 5, 6]
+];
+
+
+function flatten() {
+  // escreva seu código aqui
+  return arrays.reduce((acc, curr) => acc.concat(curr));
+}
+
+assert.deepEqual(flatten(), ["1", "2", "3", true, 4, 5, 6]);
