5933e280680114c5162851fdea140a04ad280533
Dilenio Enderle <dilenio@gmail.com>
2020-10-23 13:05:53 -0300

    3. Criando um Reducer para receber e manipular uma action

---
+++ exercises/gerenciamento-de-estado-com-redux_1/exercise3.js
@@ -0,0 +1,23 @@
+const defaultState = {
+  login: false
+};
+
+const reducer = (state = defaultState, action) => {
+  // Change code below this line
+  switch (action.type) {
+    case 'LOGIN':
+      return { login: true };
+    default:
+      return state;
+  }
+  // Change code above this line
+};
+
+const store = Redux.createStore(reducer);
+
+const loginAction = () => {
+  return {
+    type: 'LOGIN'
+  }
+};
+
